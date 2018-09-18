2fdefccf279a3d55d04966e109228e2837bd3ba2
muge <muge.hasilci@hotmail.com>
2018-09-18 17:56:03 +0100

    Test completed for divided by 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,5 @@
 def fizzbuzz(number)
-return 'fizz' if number == 3
-return 'buzz' if number == 5
+return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
 return 'fizz' if number % 3 == 0
 return 'buzz' if number % 5 == 0
 end
