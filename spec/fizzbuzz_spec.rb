c83be5be2d16e945e2335d9081c9e08a80814a3f
Reka <rekapap28@gmail.com>
2018-09-18 17:12:58 +0100

    Test: divedable by 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,7 +7,8 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  it 'returns "fizz" when pass a number that is divedable by 3' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+    expect(fizzbuzz(12)).to eq 'fizz'
   end
 end
