f0e5f5f74216e1a9e923276fd22f5f27a369c8f6
Reka <rekapap28@gmail.com>
2018-09-18 18:07:28 +0100

    returns number if not divedable by 3 or 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,6 @@
 def fizzbuzz(number)
-return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
-return 'fizz' if number % 3 == 0
-return 'buzz' if number % 5 == 0
+  return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+  return 'fizz' if (number % 3).zero?
+  return 'buzz' if (number % 5).zero?
+  number
 end
